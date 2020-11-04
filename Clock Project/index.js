b927619f3e8b33e54a73df3548d96902ab44be0b
Israel <Israel.pro1994@gmail.com>
2020-11-04 16:15:09 -0600

    clock shows also the hands are with time currency

--- Clock Project/index.js
+++ Clock Project/index.js
@@ -7,12 +7,14 @@ let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+// we are dealing with a circle so we use the degree 360
+let hrPosition = (hr * 360/12) + ( min * (360/60)/12);
+let minPosition = ( min * 360/60) + ( sec * (360/60)/60);
+let secPosition = sec*360/60; 
+
 
 // this is moving each hand with CSS USING JS
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + hrPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 
